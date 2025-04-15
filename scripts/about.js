5ceac3fcb2d27d14053f257381f56d25b8b51b7f
salehshaeer <shaheer.ch143@gmail.com>
2025-04-15 13:50:31 +0500

    faqs added

--- scripts/about.js
+++ scripts/about.js
@@ -1,12 +1,30 @@
-const collapseValues = document.getElementById('collapseValues');
-const arrowIcon = document.getElementById('arrowIcon');
+// JavaScript to toggle the arrow direction for "Our Values"
+const valuesCollapse = document.getElementById('collapseValues');
+const valuesArrowIcon = document.getElementById('arrowIcon');
 
-collapseValues.addEventListener('show.bs.collapse', () => {
-    arrowIcon.classList.remove('bi-chevron-down');
-    arrowIcon.classList.add('bi-chevron-up');
+valuesCollapse.addEventListener('show.bs.collapse', () => {
+    console.log('Our Values expanded');
+    valuesArrowIcon.classList.remove('bi-chevron-down');
+    valuesArrowIcon.classList.add('bi-chevron-up');
 });
 
-collapseValues.addEventListener('hide.bs.collapse', () => {
-    arrowIcon.classList.remove('bi-chevron-up');
-    arrowIcon.classList.add('bi-chevron-down');
+valuesCollapse.addEventListener('hide.bs.collapse', () => {
+    console.log('Our Values collapsed');
+    valuesArrowIcon.classList.remove('bi-chevron-up');
+    valuesArrowIcon.classList.add('bi-chevron-down');
+});
+
+const faqCollapse = document.getElementById('collapseFAQ');
+const faqArrowIcon = document.getElementById('faqArrowIcon');
+
+faqCollapse.addEventListener('show.bs.collapse', () => {
+    console.log('FAQ expanded');
+    faqArrowIcon.classList.remove('bi-chevron-down');
+    faqArrowIcon.classList.add('bi-chevron-up');
+});
+
+faqCollapse.addEventListener('hide.bs.collapse', () => {
+    console.log('FAQ collapsed');
+    faqArrowIcon.classList.remove('bi-chevron-up');
+    faqArrowIcon.classList.add('bi-chevron-down');
 });
